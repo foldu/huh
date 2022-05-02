9688bb5e96aa1fee3cfc5235f828e5a9b15c3f4b
foldu <foldu@protonmail.com>
2022-05-02 12:44:04 +0200

    chore: Switch to crane

--- flake.nix
+++ flake.nix
@@ -4,23 +4,22 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    naersk.url = "github:nmattia/naersk";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, naersk, flake-utils }@inputs:
+  outputs = { self, nixpkgs, crane, flake-utils }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
         huh =
           let
-            naersk-lib = naersk.lib."${system}".override {
-              cargo = pkgs.cargo;
-              rustc = pkgs.rustc;
-            };
+            craneLib = crane.lib."${system}";
           in
-          naersk-lib.buildPackage {
-            src = ./.;
-            singleStep = true;
+          craneLib.buildPackage {
+            src = self;
             nativeBuildInputs = [
               pkgs.installShellFiles
             ];
